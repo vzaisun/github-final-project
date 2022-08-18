ec10dd8071d644a5581b392a86e0fa7b7af6addf
Vijay Sundar <97884484+vzaisun@users.noreply.github.com>
2022-08-18 08:30:49 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -21,8 +21,8 @@
    echo "Enter rate of interest per year:"
    read r
    echo "Enter time period in years:"
-   read t
+   read n
 
-   s=`expr $p \* $t \* $r / 100`
+   s=`expr $p \* $t \* $n / 100`
    echo "The simple interest is: "
    echo $s
